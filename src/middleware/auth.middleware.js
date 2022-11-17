e366756d58c243096ff2168d5949d860d7234880
German Montalbetti <germontalbetti@hotmail.com>
2022-11-17 16:04:05 -0300

    [UPDATE] Comision32125 - Desafio10 - 17-11-2022

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,8 @@
 const authMiddleware = (req, res, next) => {
-    if (!req.session?.user || !req.session?.admin) { //=== 'pepe'
+
+    // para comenzar reemplazar la linea 5 por    if (req.session?.user || req.session?.admin) {
+
+    if (!req.session?.user || !req.session?.admin) {
         return res.status(401).send('<h1>No estas autorizado</h1>')
     } 
     next()
